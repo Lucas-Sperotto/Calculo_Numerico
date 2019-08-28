c6a4a8b08ff58748cee8796c237eaf37a2a338c3
Lucas-Sperotto <sperotto.lucas@gmail.com>
2019-08-28 15:04:21 -0300

    Update Exemplo2.c

--- Exemplo2.c
+++ Exemplo2.c
@@ -1,17 +1,34 @@
+/********************************************************/
+/* Código Fonte utilizado no Exemplo 2 da Aula 1 da		*/
+/* disciplina de Cálculo Numérico do Curso de Ciência	*/
+/* da Computação da UNEMAT de Alto Araguaia - MT		*/
+/* Autor: Prof. Me. Lucas Kriesel Sperotto				*/
+/* E-mail: sperotto@unemat.br							*/
+/* Compilador: GNU GCC Compiler - MinGW					*/
+/* IDE: Code::Blocks 16.01								*/
+/********************************************************/
+
+
+/****************************/
+/* Inclusão de bibliotecas	*/
+/****************************/
 #include <stdio.h>
 
+/********************/
+/* Função Principal	*/
+/********************/
 int main ()
 {
-    float s = 0.0, x = 0.1;// 0.1
-    int i = 0;
-    int MAX = 30000;
+    float s = 0.0, x = 0.1; /* declaração das variáveis (s) e (x) */
+    int i = 0; /* declaração da variável para o laço de repetição */
+    int MAX = 30000; /* declaração de varável para armazenar o número máximo de iterações */
 
-    for(i = 1; i <= MAX; i++)
+    for(i = 1; i <= MAX; i++) /* laço de repetição de 1 até o valor da variável (MAX) */
     {
-        s = s + x;
+        s = s + x; /* atribui a variável (s) o valor existente em (s) antes da atribuição somado com o valor de (x) */
     }
 
-    printf("Soma = %Lf\n ", s);
+    printf("Soma = %Lf\n ", s); /* imprime o valor da variável (s) após as MAX operações de soma */
 
-    return 0;
+    return 0; /* encerra execução */
 }
